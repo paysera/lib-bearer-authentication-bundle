2681f33861da87a5ecb2bd93fc7e584dabb842de
Pavlo Khukhrianskyi <pavlo.khukhrianskyi@paysera.com>
2024-05-30 18:11:29 +0300

    SUPPORT-91261 updated authentication failure response

--- src/Security/BearerPassportAuthenticator.php
+++ src/Security/BearerPassportAuthenticator.php
@@ -4,21 +4,19 @@ declare(strict_types=1);
 
 namespace Paysera\BearerAuthenticationBundle\Security;
 
-use Paysera\BearerAuthenticationBundle\Entity\BearerUserInterface;
 use Paysera\BearerAuthenticationBundle\Security\Token\BearerToken;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\HeaderBag;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
-use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
-use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;
 use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
 
 class BearerPassportAuthenticator extends AbstractAuthenticator
@@ -109,7 +107,7 @@ class BearerPassportAuthenticator extends AbstractAuthenticator
             $exception->getToken()
         ]);
 
-        return new Response('Invalid bearer token', Response::HTTP_UNAUTHORIZED);
+        return new JsonResponse(['error' => 'unauthorized' ,'error_description' => 'No authorization data found'], Response::HTTP_UNAUTHORIZED);
     }
 
     public function createToken(Passport $passport, string $firewallName): TokenInterface
